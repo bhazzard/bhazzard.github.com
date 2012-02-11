55eee8435ef0a9bca6f6573e76c65dac526976e4
Brian Hazzard <bhazzard@gmail.com>
2012-02-11 17:51:28 -0500

    new site

---
+++ Blog/Blog_files/Blog.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20120211
+
+setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function photocastSubscribe()
+{photocastHelper("http://brianhazzard.com/Brian_Hazzards_Blog/Blog/rss.xml");}
+function onPageLoad()
+{loadMozillaCSS('Blog_files/BlogMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
