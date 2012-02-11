d9bf3cc08aca98236c6d46fe5a2b3f0ac3e93f1f
Brian Hazzard <bhazzard@gmail.com>
2012-02-11 18:18:35 -0500

    try again :)

---
+++ Blog/Hazzards_Hypotheses/Hazzards_Hypotheses_files/Hazzards_Hypotheses.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20120211
+
+setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function photocastSubscribe()
+{photocastHelper("http://brianhazzard.com/Blog/Hazzards_Hypotheses/rss.xml");}
+function onPageLoad()
+{loadMozillaCSS('Hazzards_Hypotheses_files/Hazzards_HypothesesMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
